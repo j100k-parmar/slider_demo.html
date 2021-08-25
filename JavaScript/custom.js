fd8dcd93b7e263d49d9637120aaf251644185dd6
Jinkal Parmar <73112186+j100k-parmar@users.noreply.github.com>
2021-08-25 16:09:18 +0530

    slider

---
+++ JavaScript/custom.js
@@ -0,0 +1,11 @@
+$('.slick_slide').slick({
+    slidesToShow: 5,
+    slidesToScroll: 1,
+    // autoplay: true,
+    dots: true,
+    speed: 800,
+    autoplaySpeed: 1000,
+    prevArrow: '<i class="fas fa-angle-left left-arrow"></i>',
+    nextArrow: '<i class="fas fa-angle-right right-arrow"></i>',
+
+});
